54c4054ef8e900a631debbabc7e6b073b9abb7ee
Anubis <anubisawooo@163.com>
2021-09-19 18:27:21 +0800

    3

--- c03_common_programming_concepts/_4_comments/src/main.rs
+++ c03_common_programming_concepts/_4_comments/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    // I’m feeling lucky today
+    let lucky_number = 7; // I’m feeling lucky today
+
+    println!("the value of lucky_number is: {}", lucky_number);
 }
