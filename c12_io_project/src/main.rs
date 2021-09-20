9e5ea2c25cf71ffbdfcdc8dd9f33b3e0097f0e16
Anubis <anubisawooo@163.com>
2021-09-21 01:27:42 +0800

    12

---
+++ c12_io_project/src/main.rs
@@ -0,0 +1,30 @@
+use std::env;
+
+use std::process;
+use c12_io_project::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = c12_io_project::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+
+}
+
+
+
+
+
+
